c48212fc7bed2ae23f767fa3727b3f7fcb62ac06
fabricio jacob <fabricio.jacob@outlook.com>
2022-06-30 00:45:44 -0300

    add

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 eval $(minikube docker-env)
 mvn clean install
 mvn spring-boot:build-image
-docker push fabricio211/product-service:1.0.1
+docker push fabricio211/product-service:2.0.1
 eval $(minikube docker-env -u)
